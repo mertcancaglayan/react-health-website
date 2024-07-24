ff0fdee435f8d4b1e9a67c44fdcb7cf2cc9f1ccc
M.can <caglayan.mertcan37@gmail.com>
2024-07-24 14:38:34 +0300

    remove unused variable

--- src/data/footerContent.js
+++ src/data/footerContent.js
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faTwitter, faInstagram, faLinkedin, faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 export const FooterData = {
